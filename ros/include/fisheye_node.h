a5f7c09e73a31cc20c1988c3d3fa2bcc46670e49
Kevin ZHAO <njuptzhc@163.com>
2019-05-31 13:51:27 +0800

    First run fisheye_node

--- ros/include/fisheye_node.h
+++ ros/include/fisheye_node.h
@@ -2,12 +2,14 @@
 #define ORBSLAM2_ROS_FISHEYE_NODE_H
 
 #include <string>
+#include <fisheye_common/undistort.h>
 
 class FisheyeNode {
  public:
   void RunFromDataset();
   std::string vocabulary_path;
   std::string settings_path;
+  fisheye::FisheyeUndistort undistort;
  private:
 };
 
--- ros/src/fisheye_node.cpp
+++ ros/src/fisheye_node.cpp
@@ -3,12 +3,11 @@
 #include <chrono>
 
 void FisheyeNode::RunFromDataset() {
-  ORB_SLAM2::System SLAM(
+  ORB_SLAM2::System slam(
       vocabulary_path, settings_path, ORB_SLAM2::System::MONOCULAR);
   long now =
       std::chrono::duration_cast<std::chrono::milliseconds>(
           std::chrono::system_clock::now().time_since_epoch()).count();
-  cv::Mat Tcw;
   int id = 0;
   while(true) {
     ++id;
@@ -17,10 +16,23 @@ void FisheyeNode::RunFromDataset() {
         cv::imread(
             ("./images/mono_" + std::to_string(id) + ".jpg").c_str(),
             CV_LOAD_IMAGE_COLOR);
-
+    image = undistort.Undistort(image);
+    slam.TrackMonocular(image, now / 1000.0);
+    cv::Mat debug_image = slam.DrawCurrentFrame();
+    cv::imshow("debug", debug_image);
+    if (cv::waitKey(1) >= 0) {
+      break;
+    }
   }
+  slam.Shutdown();
 }
 
 int main() {
-
+  FisheyeNode node;
+  node.vocabulary_path = "ORBvoc.txt";
+  node.settings_path = "webcam.yaml";
+  if (!node.undistort.LoadCalibResult("calib_result.txt")) {
+    return -1;
+  }
+  node.RunFromDataset();
 }
